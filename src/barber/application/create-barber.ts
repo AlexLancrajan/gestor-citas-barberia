1fe4159ec252a0f345c28ce43960fe761bfba689
AlexLancrajan <alexlancrajan@gmail.com>
2024-12-01 21:57:11 +0100

    Minor fix bug

--- src/barber/application/create-barber.ts
+++ src/barber/application/create-barber.ts
@@ -1,11 +1,11 @@
-import { BarberInputFields } from "../domain/barber";
+import { BarberFields, BarberInputFields } from "../domain/barber";
 import { BarberRepository } from "../domain/barber-repository";
 
 
 export class CreateBarber {
   constructor(private readonly barberRepository: BarberRepository) { }
 
-  async run(barberInputFields: BarberInputFields): Promise<BarberInputFields> {
+  async run(barberInputFields: BarberInputFields): Promise<BarberFields> {
     try {
       const barber = await this.barberRepository.createBarber(barberInputFields);
       return barber;
