5a591de8f26705bf6507fccf1649a1e20bb5d4cb
AlexLancrajan <alexlancrajan@gmail.com>
2025-01-12 12:57:44 +0100

    password error info

--- src/user/infrastructure/user-schema.ts
+++ src/user/infrastructure/user-schema.ts
@@ -1,13 +1,20 @@
 import { z } from 'zod';
 import { Roles } from '../domain/user';
 
-const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&_])[A-Za-z\d@.#$!%*?&_]{6,}$/;
+const passwordSchema = z
+  .string()
+  .min(8, { message: "Password must be at least 8 characters long" })
+  .max(20, { message: "Password cannot exceed 20 characters" })
+  .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter" })
+  .regex(/[a-z]/, { message: "Password must contain at least one lowercase letter" })
+  .regex(/[0-9]/, { message: "Password must contain at least one number" })
+  .regex(/[@$!%*?&#]/, { message: "Password must contain at least one special character (@$!%*?&#)" });
 
 export const userGoogleRegistrationSchema = z.object({
   userId: z.string().trim(),
   username: z.string().trim()
     .min(5, 'Username too short, must be at least 5 characters long'),
-  password: z.string().trim().regex(passwordRegex),
+  password: passwordSchema,
   email: z.string().trim().email(),
   phone: z.string().trim().min(9, 'Invalid phone format'),
   name: z.string().trim().optional(),
@@ -19,7 +26,7 @@ export const userGoogleRegistrationSchema = z.object({
 export const userRegistrationSchema = z.object({
   username: z.string().trim()
     .min(5, 'Username too short, must be at least 5 characters long'),
-  password: z.string().trim().regex(passwordRegex),
+  password: passwordSchema,
   email: z.string().trim().email(),
   phone: z.string().trim().min(9, 'Invalid phone format'),
   name: z.string().trim().optional(),
@@ -30,14 +37,14 @@ export const userRegistrationSchema = z.object({
 
 export const userLoginSchema = z.object({
   username: z.string(),
-  password: z.string().regex(passwordRegex),
+  password: passwordSchema,
 }).strict();
 
 export const userModificationsSchema = z.object({
   username: z.string().trim()
   .min(5, 'Username too short, must be at least 5 characters long')
   .optional(),
-  password: z.string().trim().regex(passwordRegex).optional(),
+  password: passwordSchema.optional(),
   email: z.string().trim().email().optional(),
   phone: z.string().trim().min(9, 'Invalid phone format').optional(),
   name: z.string().trim().optional(),
